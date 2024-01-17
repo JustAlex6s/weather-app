784a560bd4d3e13bf5880c1f238ca620115882af
Alex6s <alexis.delfosse01@gmail.com>
2024-01-17 15:29:28 +0100

    ajout icône + css

--- script.js
+++ script.js
@@ -15,6 +15,69 @@ if (savedCity1) {
 if (savedCity2) {
   textInput2.value = savedCity2;
 }
+const weatherIcons = {
+    //thunderstom
+    'thunderstorm with light rain': './img/thunderstorm.png',
+    'thunderstorm with rain': './img/thunderstorm.png',
+    'thunderstorm with heavy rain': './img/thunderstorm.png',
+    'light thunderstorm': './img/thunderstorm.png',
+    'thunderstorm': './img/thunderstorm.png',
+    'heavy thunderstorm': './img/thunderstorm.png',
+    'ragged thunderstorm': './img/thunderstorm.png',
+    'thunderstorm with light drizzle': './img/thunderstorm.png',
+    'thunderstorm with drizzle': './img/thunderstorm.png',
+    'thunderstorm with heavy drizzle': './img/thunderstorm.png',
+    //drizzle
+    'light intensity drizzle': './img/bruime.png',
+    'drizzle': './img/bruime.png',
+    'heavy intensity drizzle': './img/bruime.png',
+    'light intensity drizzle rain': './img/bruime.png',
+    'drizzle rain': './img/bruime.png',
+    'heavy intensity drizzle rain': './img/bruime.png',
+    'shower rain and drizzle': './img/bruime.png',
+    'heavy shower rain and drizzle': './img/bruime.png',
+    'shower drizzle': './img/bruime.png',
+    //rain
+    'light rain': './img/rain.png',
+    'moderate rain': './img/rain.png',
+    'heavy intensity rain': './img/rain.png',
+    'very heavy rain': './img/rain.png',
+    'extreme rain': './img/rain.png',
+    'freezing rain': './img/mist.png',
+    'light intensity shower rain': './img/bruime.png',
+    'shower rain': './img/img/bruime.png',
+    'heavy intensity shower rain': './img/bruime.png',
+    'ragged shower rain': './img/bruime.png',
+    //snow
+    'light snow': './img/snow.png',
+    'snow': './img/snow.png',
+    'heavy snow': './img/snow.png',
+    'sleet': './img/snow.png',
+    'shower sleet': './img/snow.png',
+    'light rain and snow': './img/snow.png',
+    'rain and snow': './img/snow.png',
+    'light shower snow': './img/snow.png',
+    'shower snow': './img/snow.png',
+    'heavy shower snow': './img/snow.png',
+    //atmosphere
+    'mist': './img/mist.png',
+    'smoke': './img/mist.png',
+    'haze': './img/mist.png',
+    'sand, dust whirls': './img/mist.png',
+    'fog': './img/mist.png',
+    'sand': './img/mist.png',
+    'dust': './img/mist.png',
+    'volcanic ash': './img/mist.png',
+    'squalls': './img/mist.png',
+    'tornado': './img/mist.png',
+    //clear
+    'clear sky': './img/clear.png',
+    //clouds
+    'few clouds': './img/few_clouds.png',
+    'scattered clouds': './img/cloudy.png',
+    'broken clouds': './img/grey_clouds.png',
+    'overcast clouds': './img/grey_clouds.png',
+};
 
 // Add an event listener to the form for when the user submits
 form.addEventListener('submit', async function (e) {
@@ -59,17 +122,19 @@ async function displayWeather(containerId, forecastData) {
             const temperature = (forecast.main.temp - 273.15).toFixed(0);
             const description = forecast.weather[0].description;
             const humidity = forecast.main.humidity;
+            const iconUrl = weatherIcons[description.toLowerCase()];
             let response = await fetch (`https://api.unsplash.com/search/photos?client_id=l2psSZ7s-EII5eJw3kkWXDvdjUNlWxeNZf9kc1AeAnE&page=1&query=${cityName}`);
             let tableau = await response.json();
             console.log(tableau);
             let url = tableau.results[0].urls.small;
             const cardContent = document.createElement('div');
-            cardContent.style.backgroundImage = `url(${url})`;
+            
             cardContent.classList.add('forecast-card');
             cardContent.innerHTML = `
                 <img src="${url}" class="imgVille">
                 <h2>${cityName} le ${date.toLocaleDateString()}</h2>
                 <p>${temperature}°C</p>
+                <img src="${iconUrl}" alt="${description}" />
                 <p>${description}</p>
                 <p>Humidity: ${humidity}%</p>
             `;
